368a5707f188e51698216a9ba6dfb4a5f780660b
Hoang Sang <93880956+Sunblack00@users.noreply.github.com>
2025-03-03 21:58:39 +0700

    done

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function NotFound() {
-  return <h1>404 - Trang không tồn tại!</h1>;
+  return (
+    <div className="bg-gray-900 min-h-screen flex items-center justify-center p-4">
+      <h1 className="text-4xl font-bold text-red-500 dark:text-red-400 up">
+        404 - Trang Không Tồn Tại!
+      </h1>
+    </div>
+  );
 }
